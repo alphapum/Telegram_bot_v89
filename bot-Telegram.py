1409d6182c64a5a4f614d26d316c92fecddb28b7
alphapum <alphapum@gmail.com>
2024-10-12 01:01:26 +0700

    update Readme

--- bot-Telegram.py
+++ bot-Telegram.py
@@ -1,22 +1,10 @@
-# %% [markdown]
-# ## Telegram bot Notification 
-
-# %%
-# !pip install pyTelegramBotAPI
-
-# %%
 import telebot
 
 
 
-# %%
-# Replace 'YOUR_BOT_TOKEN' with your actual token
 bot_token = ""
 bot = telebot.TeleBot(bot_token)
 
-
-
-# %%
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
 	bot.reply_to(message, "Howdy, how are you doing?")
@@ -29,9 +17,6 @@ def help_message(message):
 def echo_all(message):
 	bot.reply_to(message, message.text)
 
-
-
-# %%
 bot.infinity_polling()
 
 
